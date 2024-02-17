157c3fb2e5fc9f8c17d838c353c024e971575793
omrzcn <omrzcnn@gmail.com>
2024-02-17 13:02:41 +0300

    problem is from feature - 10

---
+++ src/Login/Problem.java
@@ -0,0 +1,19 @@
+package Login;
+
+public class Problem {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(5);
+        }
+
+        // this line is important
+
+
+
+    }
+
+
+
+}
